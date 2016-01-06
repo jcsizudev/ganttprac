4883c67c67cdd3f1c4f4cfd7593f4c3a3275079e
jcsizudev <takeshi.izu@gmail.com>
2016-01-06 14:32:06 +0900

    取りあえず保存

---
+++ index.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var app = express();
+var publicPath = path.join(__dirname, 'public');
+
+app.use(express.static(publicPath));
+console.log(publicPath);
+
+app.get('/', function (req, res) {
+  var filePath = "";
+  var dir = fs.readdirSync(publicPath);
+  var cts = '<div>';
+  dir.filter(function (file) {
+    filePath = publicPath + '\\' + file;
+    return fs.statSync(filePath).isFile() && /.*\.html$/.test(filePath);
+  }).forEach(function (file) {
+    console.log(file);
+    cts = cts + '<div><a href="' + file +  '">' + file + '</a></div>';
+  });
+  cts = cts + '</div>';
+
+  res.send(cts);
+});
+
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
